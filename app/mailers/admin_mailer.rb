9b681fcffc79b879001a8b2bd454204dbfb7dcdc
Noah Singer <noah.singer@gmail.com>
2014-04-10 15:10:29 -0500

    improve multipart emails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,26 +1,28 @@
 class AdminMailer < ActionMailer::Base
   helper ApplicationHelper
-  
+
   def general_contact( contact )
    xxx@xxx.xxx = contact
 
-    # Implicit template rendering is not performed if any attachments or parts have been added to the email. 
-    # This means that you’ll have to manually add each part to the email and set the content type of the email 
+    # Implicit template rendering is not performed if any attachments or parts have been added to the email.
+    # This means that you’ll have to manually add each part to the email and set the content type of the email
     # to multipart/alternative.
-    
-#     :parts_order => [ "text/plain", "text/enriched", "text/html" ], 
-# 		:content_type => 'multipart/related', 
-# 		:content_type => 'multipart/alternative', 
 
-		mail( :parts_order => [ "text/plain", "text/enriched", "text/html" ], 
-					:to => APP_CONFIG['owner_email'], 
-					:subject => 'Contact Request @ Ingenio.us', 
+#     :parts_order => [ "text/plain", "text/enriched", "text/html" ],
+# 		:content_type => 'multipart/related',
+# 		:content_type => 'multipart/alternative',
+
+		if contact.attachment_file_name
+			attachments[contact.attachment_file_name] = {:content => File.read(contact.attachment.to_file.path), :mime_type => contact.attachment_content_type}
+		end
+
+		mail( :parts_order => [ "text/plain", "text/enriched", "text/html" ],
+					:to => APP_CONFIG['owner_email'],
+					:subject => 'Contact Request @ Ingenio.us',
 					:from => contact.return_email) do |format|
-		  format.html
+# 		  format.html
 		  format.text
 		end
-		
-		attachments[contact.attachment_file_name] = {:content => File.read(contact.attachment.to_file.path), :mime_type => contact.attachment_content_type} if contact.attachment_file_name
     
 #     attachments[contact.attachment_file_name] = {:content => File.read(contact.attachment.to_file.path), :mime_type => contact.attachment_content_type} if contact.attachment_file_name
 #     mail( :to => APP_CONFIG['owner_email'], :subject => 'Contact Request @ Ingenio.us', :from => contact.return_email )
