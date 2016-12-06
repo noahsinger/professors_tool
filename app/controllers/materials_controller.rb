36a8c15dd8057f19589cec290fb158e1cf0b3e71
Noah Singer <noah.singer@gmail.com>
2016-12-06 08:12:38 -0600

    cleaning up value passing to paritals and other fixes

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,10 +1,14 @@
 class MaterialsController < ApplicationController
   before_action :load_course
-  
+
   def load_course
+    if params[:section_id]
+     xxx@xxx.xxx = Section.find params[:section_id]
+    end
+
    xxx@xxx.xxx = Course.find( params[:course_id] )
   end
-  
+
   # GET /materials
   # GET /materials.xml
   def index
