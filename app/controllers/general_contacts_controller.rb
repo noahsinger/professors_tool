3c352a6c86864653f5c301a9ae648d0eb15bee7b
Noah Singer <noah.singer@gmail.com>
2016-12-09 16:13:19 -0600

    XHR submission for general contact form(s)

--- app/controllers/general_contacts_controller.rb
+++ app/controllers/general_contacts_controller.rb
@@ -10,7 +10,7 @@ class GeneralContactsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml xxx@xxx.xxx }
+      format.xml  { render xxx@xxx.xxx }
     end
   end
 
@@ -29,15 +29,20 @@ class GeneralContactsController < ApplicationController
         flash[:notice] = 'Contact Email was successfully sent.'
         
         xxx@xxx.xxx
+          @status = xxx@xxx.xxx
           format.html { redirect_to( xxx@xxx.xxx ) }
         else
+         xxx@xxx.xxx = root_url
           format.html { redirect_to( root_url ) }
         end
         
-        format.xml  { render :xml => @general_contact, :status => :created, :location xxx@xxx.xxx }
+        format.js
+        format.xml  { render xml: @general_contact, status: :created, xxx@xxx.xxx }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml xxx@xxx.xxx :status => :unprocessable_entity }
+       xxx@xxx.xxx = "failed"
+        format.js
+        format.html { render action: "new" }
+        format.xml  { render xxx@xxx.xxx status: :unprocessable_entity }
       end
     end
   end
