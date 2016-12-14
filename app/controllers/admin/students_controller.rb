8cafc5043778fc375e6c80b5c276c51246c45c72
Noah Singer <noah.singer@gmail.com>
2016-12-14 10:08:00 -0600

    blockified students

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,27 +1,27 @@
 class Admin::StudentsController < ApplicationController
   before_action :authenticate
-  
-  before_action :load_semester, :only => [:new, :create, :unenroll]
-  before_action :load_section, :only => [:new, :create, :unenroll]
-    
+
+  before_action :load_semester, only: [:new, :create, :unenroll]
+  before_action :load_section, only: [:new, :create, :unenroll]
+
   def load_semester
    xxx@xxx.xxx = Semester.find(params[:semester_id])
   end
-  
+
   def load_section
    xxx@xxx.xxx = Section.find(params[:section_id])
   end
-  
+
   def index
 	  if params['q']
-xxx@xxx.xxx = Student.search(params['q'])  
+xxx@xxx.xxx = Student.search(params['q'])
 	  else
   	 xxx@xxx.xxx = Student.paginate(params['page'] || 0).order('last_name')
   	end
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml xxx@xxx.xxx }
+      format.xml  { render xxx@xxx.xxx }
     end
   end
 
@@ -30,7 +30,7 @@ xxx@xxx.xxx = Student.search(params['q'])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml xxx@xxx.xxx }
+      format.xml  { render xxx@xxx.xxx }
     end
   end
 
@@ -40,7 +40,7 @@ xxx@xxx.xxx = Student.search(params['q'])
     if session[:email]
      xxx@xxx.xxx = session[:email]
       session[:email] = nil
-      
+
       # extract the first and last name from the email address
       d = @student.email.match xxx@xxx.xxx
       if d
@@ -51,7 +51,7 @@ xxx@xxx.xxx = Student.search(params['q'])
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml xxx@xxx.xxx }
+      format.xml  { render xxx@xxx.xxx }
     end
   end
 
@@ -61,25 +61,28 @@ xxx@xxx.xxx = Student.search(params['q'])
 
   def create
    xxx@xxx.xxx = Student.new(allowed_params)
-    
+
     respond_to do |format|
       xxx@xxx.xxx
        xxx@xxx.xxx = Enrollment.new
         @enrollment.student_id xxx@xxx.xxx
         @enrollment.section_id xxx@xxx.xxx
-        
+
        xxx@xxx.xxx = EnrollmentStatus.where("name = 'Enrolled'").first
         @enrollment.enrollment_status xxx@xxx.xxx
-        
+
        xxx@xxx.xxx
-        
+
+        @status = xxx@xxx.xxx @section)
         flash[:notice] = 'Student was successfully created and enrolled.'
         format.html { xxx@xxx.xxx }
-        format.xml  { render :xml => @student, :status => :created, :location xxx@xxx.xxx }
+        format.xml  { render xml: @student, status: :created, xxx@xxx.xxx }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml xxx@xxx.xxx :status => :unprocessable_entity }
+       xxx@xxx.xxx = "failed"
+        format.html { render action: "new" }
+        format.xml  { render xxx@xxx.xxx status: :unprocessable_entity }
       end #if save
+      format.js
     end #respond_to
   end
 
@@ -88,13 +91,16 @@ xxx@xxx.xxx = Student.search(params['q'])
 
     respond_to do |format|
       xxx@xxx.xxx
+        @status = xxx@xxx.xxx
         flash[:notice] = 'Student was successfully updated.'
         format.html { xxx@xxx.xxx }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml xxx@xxx.xxx :status => :unprocessable_entity }
+       xxx@xxx.xxx = "failed"
+        format.html { render action: "edit" }
+        format.xml  { render xxx@xxx.xxx status: :unprocessable_entity }
       end
+      format.js
     end
   end
 
@@ -103,11 +109,13 @@ xxx@xxx.xxx = Student.search(params['q'])
    xxx@xxx.xxx
 
     respond_to do |format|
+     xxx@xxx.xxx = admin_students_url
       format.html { redirect_to(admin_students_url) }
       format.xml  { head :ok }
+      format.js
     end
   end
-  
+
   def allowed_params
     params.require(:student).permit(:first_name, :last_name, :email, :middle_name, :goes_by)
   end
