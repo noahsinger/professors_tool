3c508fdbc1077d7bb703fb39e5b2339869e81976
Noah Singer <noah.singer@gmail.com>
2017-01-18 14:29:36 -0600

    adds a delay so screen resizing has a chance to  re-layout

--- app/assets/javascripts/flying_blocks.js
+++ app/assets/javascripts/flying_blocks.js
@@ -12,19 +12,20 @@ NodeList.prototype.includes = function( other_node ) {
 
 // FLYING BLOCKS ------------------------
 var FlyingBlocks = {
-	// store all blocks on the current page to be compared with new blocks loaded to 
-	// determine what's new and will apprear, what's the same and will stay, and
+	// store all blocks on the current page to be compared with new blocks loaded to
+	// determine what's new and will appear, what's the same and will stay, and
 	// what is leaving and will disappear
 	current_blocks : [],
-	
-	
+	refresh_timer : null,
+
+
 	on : function( ) {
 		// NOT CURRENTLY USED
-		document.addEventListener("turbolinks:click", function() {console.log("---------------------------------------------\nclick")});
-		document.addEventListener("turbolinks:before-visit", function() {console.log("before-visit")});
-		document.addEventListener("turbolinks:visit", function() {console.log("visit")});
-		document.addEventListener("turbolinks:request-start", function() {console.log("request-start")});
-		document.addEventListener("turbolinks:request-end", function() {console.log("request-end")});
+		// document.addEventListener("turbolinks:click", function() {console.log("---------------------------------------------\nclick")});
+		// document.addEventListener("turbolinks:before-visit", function() {console.log("before-visit")});
+		// document.addEventListener("turbolinks:visit", function() {console.log("visit")});
+		// document.addEventListener("turbolinks:request-start", function() {console.log("request-start")});
+		// document.addEventListener("turbolinks:request-end", function() {console.log("request-end")});
 
 		// READY
 		$(document).ready(function( ) {
@@ -35,7 +36,7 @@ var FlyingBlocks = {
 		//RESIZE
 		$(window).resize(function( ) {
 			console.log("--- resized");
-			FlyingBlocks.refresh( );
+			FlyingBlocks.delayed_refresh( );
 		});
 
 		// LOAD
@@ -46,16 +47,16 @@ var FlyingBlocks = {
 
 		// RENDER
 		document.addEventListener("turbolinks:render", FlyingBlocks.render);
-		
-		
+
+
 		FlyingBlocks.init( );
-	}, //end on 
-	
-	
+	}, //end on
+
+
 	off : function( ) {
 		console.log("shutting down isotope");
 		$('#page').isotope('destroy');
-		
+
 		// LOAD
 		document.removeEventListener("turbolinks:load", FlyingBlocks.load);
 
@@ -65,8 +66,8 @@ var FlyingBlocks = {
 		// RENDER
 		document.removeEventListener("turbolinks:render", FlyingBlocks.render);
 	}, //end off
-	
-	
+
+
 	//INIT
 	init : function( ) {
 		$('#page').isotope({
@@ -81,37 +82,37 @@ var FlyingBlocks = {
 
 		FlyingBlocks.record_current_blocks( );
 	}, //end init
-	
-	
+
+
 	//RECORD CURRENT BLOCKS
 	record_current_blocks : function( ) {
 		FlyingBlocks.current_blocks = document.querySelectorAll(".block");
 		// console.log("- current_blocks: ");
 		// console.log(current_blocks);
 	}, //end record_current_blocks
-	
-	
+
+
 	//LOAD
 	load : function( ) {
 		console.log("load");
-	
+
 		FlyingBlocks.record_current_blocks( );
 
-		$(".block").click(function( ) {
-			$(".block--width2").each(function( ) {
-				this.className = this.className.replace(/\b\s?(block--width2)\b/g, '');
-			});
-
-			if( ! /grid-sizer/.test(this.className) && ! /form/.test(this.className) ) {
-				this.className += " block--width2";
-			}
-		});
+		// $(".block").click(function( ) {
+		// 	$(".block--width2").each(function( ) {
+		// 		this.className = this.className.replace(/\b\s?(block--width2)\b/g, '');
+		// 	});
+		//
+		// 	if( ! /grid-sizer/.test(this.className) && ! /form/.test(this.className) ) {
+		// 		this.className += " block--width2";
+		// 	}
+		// });
 
 		FlyingBlocks.refresh( );
 		FlyingBlocks.init_elements( );
 	}, //end load
-	
-	
+
+
 	//BEFORE_RENDER
 	before_render : function(event) {
 		console.log("before-render");
@@ -142,8 +143,8 @@ var FlyingBlocks = {
 			}
 		});
 	}, //end before_render
-	
-	
+
+
 	//RENDER
 	render : function( ) {
 		console.log("render");
@@ -151,7 +152,6 @@ var FlyingBlocks = {
 		console.log("- " + $(".appear").size( ) + " nodes to appear");
 		console.log("- " + $(".disappear").size( ) + " nodes to disappear");
 
-		// $(".appear").fadeIn("slow");
 		$("#page").isotope('insert', $(".appear"));
 
 		$(".disappear").fadeOut("slow", function( ) {
@@ -168,8 +168,8 @@ var FlyingBlocks = {
 			this.className = this.className.replace(/\b\s?((dis)?appear)\b/g, '');
 		});
 	}, //end render
-	
-	
+
+
 	//PROCESS FORM
 	process_form : function( form_partial, success_url ) {
 		Turbolinks.clearCache();
@@ -182,13 +182,14 @@ var FlyingBlocks = {
 			if(success_url == 'failed') {
 				//alert("failed");
 				$('#page').append(form_partial);
-				console.log('-- appended');	
+				console.log('-- appended');
 				$('#page').isotope('insert', $('.form'));
-	
+
 				//new/modified form block won't be removed on next nav unless it's in current blocks so rebuild current_blocks here
 				FlyingBlocks.record_current_blocks( );
-				FlyingBlocks.refresh( );
 				FlyingBlocks.init_elements( );
+				FlyingBlocks.refresh( );
+
 			} else {
 			  // alert("sent!");
 				console.log("-- visiting " + success_url);
@@ -196,8 +197,8 @@ var FlyingBlocks = {
 			}
 		});
 	}, //end process_form
-	
-	
+
+
 	//INIT_ELEMENTS
 	init_elements : function( ) {
 		//prep remoteipart forms
@@ -206,8 +207,8 @@ var FlyingBlocks = {
 			console.log("remotipart complete:");
 		  console.log(e, data);
 		});
-		
-		
+
+
 		$(".block").each( function( ) {
 			//look for and execute any scripts included with blocks
 			if( $(this).data("script") ) {
@@ -215,12 +216,48 @@ var FlyingBlocks = {
 				eval($(this).data("script"));
 			}
 		});
+
+		// $(".block").css("transform", "scale(1)");
+		//
+		// var subjects = [];
+		// var scale_factor = 15;
+		// //get all of the full names from the current URL (in order)
+		// window.location.pathname.match(/\/(\w+)\/?/g).forEach(function(val) {
+		// 	subjects.push(val.replace(/\//g,''));
+		// });
+		//
+		// for (var i = 0 ; i < subjects.length ; i++) {
+		// 	var classname = "." + subjects[i];
+		// 	$(classname).each(function( ) {
+		// 		var scale = 1.15-(((subjects.length - i) * scale_factor)/100.0);
+		// 		console.log("scaling " + classname + " to " + scale)
+		// 		// $(this).css("transform", "scale(" + scale + ")");
+		// 		$(this).addClass("smaller");
+		// 	});
+		// }
+
 	}, //end init_elements
-	
-	
+
+
 	//REFRESH
 	refresh : function( ) {
 		console.log("- refreshing layout");
+
+		FlyingBlocks.sort( );
+		FlyingBlocks.update_layout( );
+	}, //end refresh
+
+
+	//DELAYED REFRESH
+	delayed_refresh : function( ) {
+		console.log("- refreshing layout (delayed)");
+
+		if(FlyingBlocks.refresh_timer == null)
+			FlyingBlocks.refresh_timer = setTimeout("FlyingBlocks.update_layout( )", 1500);
+	}, //end refresh
+
+	//SORT
+	sort : function( ) {
 		$("#page").isotope({
 			getSortData: {
 		    active: function( block ) {
@@ -232,7 +269,7 @@ var FlyingBlocks = {
 					}
 		    }
 			},
-			
+
 			// sortAscending: {
 			// 	active: true
 			// },
@@ -240,6 +277,12 @@ var FlyingBlocks = {
 			sortBy: [ 'active' ]
 		});
 		$("#page").isotope('updateSortData');
+	}, //end sort
+
+
+	//UPDATE_LAYOUT
+	update_layout : function( ) {
 		$("#page").isotope('layout');
-	}
+		FlyingBlocks.refresh_timer = null;
+	} //end update_layout
 }; //end FlyingBlocks
