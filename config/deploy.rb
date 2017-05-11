b7a49941013cfdf6bd416503bcd42b0fb12e8b6b
Noah Singer <noah.singer@gmail.com>
2017-05-10 19:17:07 -0500

    tweeted deploy that should work better for my setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,12 +12,11 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_bind,       "unix://home/#{fetch(:user)}/#{fetch(:application)}/shared/sockets/puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_state,      "/tmp/pids/#{fetch(:application)}-puma.state"
+set :puma_pid,        "/tmp/pids/#{fetch(:application)}-puma.pid"
+set :puma_access_log, "/home/#{fetch(:user)}/#{fetch(:application)}/log/puma.error.log"
+set :puma_error_log,  "/home/#{fetch(:user)}/#{fetch(:application)}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -38,8 +37,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir /tmp/sockets -p"
+      execute "mkdir /tmp/pids -p"
     end
   end
 
